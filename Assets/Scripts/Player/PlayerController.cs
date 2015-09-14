a2f9ae0f0ee9d7844e31f017a98d4bcca76abd35
Haruna Kaze <haruna.kaze@gmail.com>
2015-09-14 20:18:01 +0700

    + Mouth open animation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,19 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-	public Image touchDetector;
+	public TouchDetector touchDetector;
+
+	private Animator animator;
+	private int isOpen;
 
 	void Start ()
 	{
-	
+		animator = GetComponent<Animator> ();
+		isOpen = Animator.StringToHash ("isOpen");
 	}
 
 	void Update ()
 	{
-	
+		animator.SetBool(isOpen, touchDetector.isClicked);
 	}
 }
